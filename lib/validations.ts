d62153baedf730a12a0ca10c6efbe99756f35e1c
jhersy143 <jhersyafernandezit@gmail.com>
2025-02-17 22:54:17 +0800

    question form added

--- lib/validations.ts
+++ lib/validations.ts
@@ -1,19 +1,19 @@
-import { z } from 'zod';
+import { z } from "zod";
 
 export const SignInSchema = z.object({
-    email:z
+  email: z
     .string()
-    .min(1, {message: "Email is required"})
-    .email({message: "Invalid email address"}),
+    .min(1, { message: "Email is required" })
+    .email({ message: "Please provide a valid email address." }),
 
-    password:z
+  password: z
     .string()
-    .min(6, {message: "Password must be at least 6 characters long."})
-    .max(100, {message:"Password cannot exceed 100 characters."})
-})
+    .min(6, { message: "Password must be at least 6 characters long. " })
+    .max(100, { message: "Password cannot exceed 100 characters." }),
+});
 
 export const SignUpSchema = z.object({
-    username: z
+  username: z
     .string()
     .min(3, { message: "Username must be at least 3 characters long." })
     .max(30, { message: "Username cannot exceed 30 characters." })
@@ -48,5 +48,22 @@ export const SignUpSchema = z.object({
     .regex(/[^a-zA-Z0-9]/, {
       message: "Password must contain at least one special character.",
     }),
+});
 
-})
+export const AskQuestionSchema = z.object({
+  title: z
+    .string()
+    .min(5, { message: "Title is required." })
+    .max(100, { message: "Title cannot exceed 100 characters." }),
+
+  content: z.string().min(1, { message: "Body is required." }),
+  tags: z
+    .array(
+      z
+        .string()
+        .min(1, { message: "Tag is required." })
+        .max(30, { message: "Tag cannot exceed 30 characters." })
+    )
+    .min(1, { message: "At least one tag is required." })
+    .max(3, { message: "Cannot add more than 3 tags." }),
+});
